f4a6fd14fdcfdda393be48b43f1588eb050bb3cf
yfur1004 <furuse@eom.mech.eng.osaka-u.ac.jp>
2016-11-11 19:21:30 +0900

    Add numpy xxxx
    Add numpy samplecode

--- sample.py
+++ sample.py
@@ -3,3 +3,9 @@ print("Hello, Wordl!");
 print("Hello, Hello, Hello");
 
 print("Hello, Hello, Hello, Hello, Hello");
+
+import numpy as np
+a = np.array([1,2,3])
+b = np.array([0,1,2])
+c = np.dot(a, b)
+print(c)
