87b36a2bb9f7905b2c123539951163a74c93abc2
yfur1004 <furuse@eom.mech.eng.osaka-u.ac.jp>
2016-11-11 20:56:41 +0900

    Sample code
    Sample code of c

---
+++ c_exercise.c
@@ -0,0 +1,32 @@
+/*
+ * C言語のサンプルプログラム - Webkaru
+ * - 1から1000までの完全数を表示 -
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+  int i, j, amari;
+
+  /* 和 */
+  int sum;
+
+  for( i=1; i<=1000; ++i ) {
+    sum = 0;
+
+    for( j=1; j<i; ++j) {
+      if( (i % j) == 0)
+        sum += j;
+    }
+
+    /* 完全数かどうかを判定 */
+    if( j == sum )
+      printf("%d ", j);
+
+  }
+
+  printf("\n");
+
+  return 0;
+}
